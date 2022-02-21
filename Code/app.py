81eaab97ba7850a923301e2b9be9a12be0c5622d
alalalalaki <harlan.zhu@gmail.com>
2022-02-21 22:13:39 +0900

    Finish code

--- Code/app.py
+++ Code/app.py
@@ -1,3 +1,114 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import os
+import joblib
+from nltk.stem.snowball import SnowballStemmer
+from train import stemming, find_top_predict
+
+st.set_page_config(page_title=None, page_icon=None, layout='centered', initial_sidebar_state='collapsed')
+
+"""
+# Econ JEL Code Match
+
+"""
+
+
+@st.cache(show_spinner=False, allow_output_mutation=True)
+def load_file_cached(timestamp):
+    jel_des = pd.read_pickle("Data/jel_des.pkl")
+    jel_labels = joblib.load("Data/jel_labels.pkl")
+    vectorizer, classifier = joblib.load("Data/model.pkl")
+    return jel_des, jel_labels, vectorizer, classifier
+
+
+def load_file():
+    update_timestamp = os.path.getmtime("Data/model.pkl")
+    return load_file_cached(update_timestamp)
+
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+
+def hide_right_menu():
+    # ref: https://discuss.streamlit.io/t/how-do-i-hide-remove-the-menu-in-production/362/3
+    hide_streamlit_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    </style>
+    """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
+def predict(abstract, vectorizer, classifier, jel_labels, top_n):
+    stemmer = SnowballStemmer("english")
+    abstract = stemming(abstract, stemmer)
+    X = vectorizer.transform([abstract])
+    y_predict_jel, _ = find_top_predict(classifier, X, jel_labels, top_n=top_n)
+    return y_predict_jel
+
+
+def show_jels(y_predict_jel, jel_des):
+    # for jel in y_predict_jel:
+    #     if jel in jel_des.index.values:
+    #         des = jel_des.loc[jel]
+    #         st.markdown(f'{jel}: {des.des} - {des.d2c}: {des.d2d}')
+
+    # @ grouped show
+    jels = [jel for jel in y_predict_jel if jel in jel_des.index.values]
+    dess = jel_des.loc[jels]
+    for d2c, dess_group in dess.groupby("d2c", sort=False):
+        st.markdown(f"{d2c}: {dess_group.d2d.unique()[0]}")
+        for d3c, row in dess_group.iterrows():
+            f" - {d3c}: {row['des']}"
+
+
+def sidebar_info():
+    st.sidebar.header("About")
+    st.sidebar.markdown("""
+    <div style="font-size: small;">
+    This is a simple app to match JEL codes for economics papers based on the abstract text.<br>
+    It uses a simple logistic regression model trained on real economics publications of major economics jounrals in last 20 years.<br>
+    It shows the top 20 JEL codes predicted by the model and currectly have over 70 percent accuracy.<br>
+    <font style="color:green;">If you feel that the results are not satisfactory, you can try adding more texts from your paper or even directly adding the keywords that you are looking for.</font>
+    </div>
+    """, unsafe_allow_html=True)  # Author: Xuanli Zhu.<br>
+
+    st.sidebar.header("Config")
+    top_n = st.sidebar.number_input('# of JEL codes predicted', value=20, min_value=10, max_value=50)
+
+    st.sidebar.header("Report Issues")
+    st.sidebar.markdown("""
+    <div style="font-size: small">
+    Report an issue or comment at <a href="https://github.com/Alalalalaki/Econ-JEL-Match">github repo</a>
+    </div>
+    """, unsafe_allow_html=True)
+    return top_n
+
+
+def main():
+    top_n = sidebar_info()
+    hide_right_menu()
+    local_css("Code/style.css")
+
+    form = st.form(key='search')
+
+    abstract = form.text_area('Enter Abstract', height=300)
+    button_clicked = form.form_submit_button(label='Match')
+
+    jel_des, jel_labels, vectorizer, classifier = load_file()
+
+    jel_load_state = st.empty()
+
+    if button_clicked:
+        jel_load_state.markdown('Matching ...')
+        y_predict_jel = predict(abstract, vectorizer, classifier, jel_labels, top_n)
+        jel_load_state.markdown('Matched JEL Codes:')
+        show_jels(y_predict_jel, jel_des)
+
+
+if __name__ == '__main__':
+    main()
--- Code/train.py
+++ Code/train.py
@@ -1,2 +1,122 @@
 import numpy as np
 import pandas as pd
+from nltk.stem.snowball import SnowballStemmer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, TfidfTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.linear_model import LogisticRegression
+import joblib
+
+
+def stemming(sentence, stemmer):
+    stemSentence = ""
+    for word in sentence.split():
+        stem = stemmer.stem(word)
+        stemSentence += stem
+        stemSentence += " "
+    stemSentence = stemSentence.strip()
+    return stemSentence
+
+
+def prepare_date():
+    # https://media.githubusercontent.com/media/Alalalalaki/Econ-Paper-Search/main/Data/papers.csv
+    df = pd.read_csv("https://github.com/Alalalalaki/Econ-Paper-Search/blob/main/Data/papers.csv?raw=true")
+
+    # borrow from Econ-Paper-Search
+    # drop book reviews (not perfect)
+    masks = [~df.title.str.contains(i, case=False, regex=False) for i in ["pp.", " p."]]  # "pages," " pp "
+    mask = np.vstack(masks).all(axis=0)
+    df = df.loc[mask]
+    # drop some duplicates due to weird strings in authors and abstract
+    df = df[~df.duplicated(['title', 'url']) | df.url.isna()]
+
+    mask = ~df.jel.isna()
+    mask2 = df.year > 2000
+    mask3 = ~df.abstract.isna()
+
+    df = df.loc[mask & mask2 & mask3]
+
+    jel_dummy_matrix = df.jel.str.get_dummies(sep="&")
+    # remove not 2-digit jel
+    jel_dummy_matrix = jel_dummy_matrix.loc[:, ~(jel_dummy_matrix.columns.str.len() < 3)]
+    # remove label with too less entry
+    label_idx_minor = jel_dummy_matrix.sum(axis=0) <= 1
+    label_idx_minor.sum()
+    jel_dummy_matrix = jel_dummy_matrix.loc[:, ~label_idx_minor]
+    jel_labels = jel_dummy_matrix.columns.values
+
+    # further remove papers with no jel now
+    mask = jel_dummy_matrix.sum(axis=1) == 0
+    jel_dummy_matrix = jel_dummy_matrix.loc[~mask, :]
+    df = df.loc[~mask, :]
+
+    stemmer = SnowballStemmer("english")
+    abstract = df.abstract.apply(stemming, args=(stemmer,))
+
+    vectorizer = TfidfVectorizer(stop_words='english', min_df=2,
+                                 # strip_accents='unicode',
+                                 # ngram_range=(1,2), norm='l2',
+                                 )
+    token_matrix = vectorizer.fit_transform(abstract)
+
+    # y_train, y_test, X_train, X_test = train_test_split(jel_dummy_matrix, token_matrix,
+    #                             random_state=46,
+    #                             test_size=0.005, shuffle=True)
+    y_train, y_test, X_train, X_test = jel_dummy_matrix, None, token_matrix, None
+    return vectorizer, y_train, y_test, X_train, X_test, jel_labels
+
+def find_top_predict(classifier, X_individual, jel_labels, top_n=20):
+    predict_proba_ = classifier.predict_proba(X_individual)
+    predict_proba_ = np.array([a[0][1] for a in predict_proba_])
+    jel_rank = np.argsort(predict_proba_)[::-1]
+    if top_n <= 1:
+        top_n = (predict_proba_ > top_n).sum()
+    y_predict_jel = jel_labels[jel_rank][:top_n]
+    y_predict_jel_proba = predict_proba_[jel_rank][:top_n]
+    return y_predict_jel,y_predict_jel_proba
+
+def custom_metric(classifier, X_test, y_test, jel_labels, top_n=20):
+    find_rates = []
+    for i in range(len(y_test)):
+        X_test_ = X_test[i, :]
+        y_test_ = y_test.iloc[i, :]
+        y_test_jel = y_test_[y_test_ > 0].index.values
+
+        y_predict_jel, _ = find_top_predict(classifier, X_test_, jel_labels, top_n=top_n)
+
+        base = len(y_test_jel)
+        correct_find = len(set(y_predict_jel) & set(y_test_jel))
+        find_rate = correct_find/base
+        find_rates.append(find_rate)
+    avg_find_rate = np.array(find_rates).mean()
+    print(f"custom metric top_n={top_n}: {avg_find_rate}")
+    return avg_find_rate
+
+
+def train_model(X_train, y_train):
+    lr = LogisticRegression(solver='saga', max_iter=1000, C=1,
+                            class_weight="balanced", penalty="l1",  # "elasticnet"
+                            )
+    classifier = MultiOutputClassifier(lr, n_jobs=-1)
+
+    classifier.fit(X_train, y_train)
+    return classifier
+
+
+def main():
+    vectorizer, y_train, y_test, X_train, X_test,  jel_labels = prepare_date()
+    classifier = train_model(X_train, y_train)
+    custom_metric(classifier, X_test, y_test,  jel_labels, top_n=10)
+    custom_metric(classifier, X_test, y_test,  jel_labels, top_n=20)
+    custom_metric(classifier, X_test, y_test,  jel_labels, top_n=25)
+
+    joblib.dump(jel_labels, "../Data/jel_labels.pkl")
+    joblib.dump([vectorizer, classifier], "../Data/model.pkl")
+
+    # load
+
+
+
+if __name__ == '__main__':
+    main()
