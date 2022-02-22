74a68099d5018f03c7889f187fbf3d431f0f3610
alalalalaki <harlan.zhu@gmail.com>
2022-02-22 19:32:03 +0900

    Solve pkl loading problem by deploying on py38

--- Code/train.py
+++ Code/train.py
@@ -113,8 +113,8 @@ def main():
     # custom_metric(classifier, X_test, y_test,  jel_labels, top_n=20)
     # custom_metric(classifier, X_test, y_test,  jel_labels, top_n=25)
 
-    joblib.dump(jel_labels, "../Data/jel_labels.pkl", protocol=4)
-    joblib.dump([vectorizer, classifier], "../Data/model.pkl", protocol=4, compress=3)
+    joblib.dump(jel_labels, "../Data/jel_labels.pkl")
+    joblib.dump([vectorizer, classifier], "../Data/model.pkl", compress=3)
 
 
 if __name__ == '__main__':
